7a40decc1484b1a2a1923e050aa04dbed7b5cd52
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2026-02-22 15:50:05 +0100

    removed dms

--- niri/dot-config/niri/autostart.sh
+++ niri/dot-config/niri/autostart.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#waybar >/dev/null 2>&1 &
+waybar >/dev/null 2>&1 &
 
-#swaybg -i ~/.local/share/wallpapers/Desert_Color3.png >/dev/null 2>&1 &
+swaybg -i ~/.local/share/wallpapers/Desert_Color3.png >/dev/null 2>&1 &
 
 ghostty >/dev/null 2>&1 &
 
