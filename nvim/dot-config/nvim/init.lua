3013620c7a5e88a75b3d0b719685092116650c5c
Felix <65034189+MajorDisgrace@users.noreply.github.com>
2026-02-23 20:52:32 +0100

    set some settings for tabs in nvim

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -110,6 +110,11 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- tabs
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
